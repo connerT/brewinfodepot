db0f93d02348f098289fed5e604b12e3a6bab895
unknown <tolleycx@DALSAINAMX-W7D1.risk.regn.net>
2013-07-16 14:08:45 -0500

    First commit of brew app

---
+++ app.rb
@@ -0,0 +1,37 @@
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/activerecord'
+require './config/environments' #database configuration
+require './models/model'
+
+get '/' do
+	erb :index
+end 
+
+post '/submit' do
+	@model = Model.new(params[:model])
+	if @model.save
+		redirect '/models'
+	else
+		"Sorry, there was an error!"
+	end 
+end
+
+get '/models' do
+	@models = Model.all
+	erb :models
+end
+
+get '/about' do
+	erb :about
+end
+
+get '/contact' do
+	erb :contact
+end
+
+get '/random' do
+	#search for a random brew
+	erb :brew
+end
+	
